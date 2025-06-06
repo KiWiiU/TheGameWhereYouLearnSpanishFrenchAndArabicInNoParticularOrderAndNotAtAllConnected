106209c1b86e624bb27b6f38ed971eb2e5f76e04
Liam O <lioconnell37@gmail.com>
2025-06-06 13:21:35 -0400

    ping pong for events

--- TocaTalk/Assets/Scripts/Lesson1/Events.cs
+++ TocaTalk/Assets/Scripts/Lesson1/Events.cs
@@ -8,94 +8,110 @@ public class Events : MonoBehaviour
 
     public DialogueManager dm;
     public QuizManager quizManager;
-    public DialogueList[] firstDialogue;
+    public DialogueList[] dialogue;
     public Question[] test;
-    private int currentEvent;
-    private GameObject dialogueObj;
-
+    // private int currentEvent;
+    // private GameObject dialogueObj;
+    // private int currentEvent;
     private int playerScore;
     private int totalScore;
+
+    private int q, d;
     public void Start()
     {
+        q = 0;
+        d = 0;
         playerScore = 0;
         totalScore = 0;
-        currentEvent = 0;
-        // dm.OnDialogueEnd += OnDialogueEnd;
-        // quizManager.OnQuizEnd += OnQuizEnd;
-        dialogueObj = GameObject.FindWithTag("Dialogue");
+        // currentEvent = 0;
+        dm.OnDialogueEnd += OnDialogueEnd;
+        quizManager.OnQuizEnd += OnQuizEnd;
+        // dialogueObj = GameObject.FindWithTag("Dialogue");
     }
 
-    public void doThing(bool mode)
+    private void OnQuizEnd()
     {
-        int q = 0, d = 0;
-        if (mode)
-            while (true)
-            {
-                if (q >= test.Length && d >= firstDialogue.Length)
-                    break;
-                if (d < firstDialogue.Length)
-                    dm.StartDialogue(firstDialogue[d..d+1]);
-                if (q < test.Length)
-                    quizManager.StartQuiz(test[q..q+1]);
-                playerScore += quizManager.NumCorrect;
-                totalScore += quizManager.NumQuestions;
-                q++;
-                d++;
-            }
-        else
-        {
-            while (true)
-            {
-                if (q >= test.Length && d >= firstDialogue.Length)
-                    break;
-                if (q < test.Length)
-                    quizManager.StartQuiz(test[q..q+1]);
-                if (d < firstDialogue.Length)
-                    dm.StartDialogue(firstDialogue[d..d+1]);
-                playerScore += quizManager.NumCorrect;
-                totalScore += quizManager.NumQuestions;
-                q++;
-                d++;
-            }
-        }
+        playerScore += quizManager.NumCorrect;
+        totalScore += quizManager.NumQuestions;
+        NextDialogue();
     }
+
+    public void NextDialogue()
+    {
+        if (d >= dialogue.Length)
+        {
+            EndLesson();
+            return;
+        }
+        dm.StartDialogue(dialogue[d..(d + 1)]);
+        d++;
+    }
+
+    public void NextQuiz()
+    {
+        if (q >= test.Length)
+        {
+            EndLesson();
+            return;
+        }
+        quizManager.StartQuiz(test[q..(q + 1)]);
+        q++;
+    }
+    private void OnDialogueEnd()
+    {
+        NextQuiz();
+    }
+
+    private void EndLesson()
+    {
+        if((double)playerScore/totalScore >= .8)
+            Holder.progress(1);
+        else
+            Debug.Log("Player has failed the lesson.");
+            GetComponent<SceneSwap>().SwapScene("Lessons");
+    }
+
+
+    // public void doThing(bool mode)
+    // {
+    //     int q = 0, d = 0;
+    //     if (mode)
+    //         while (true)
+    //         {
+    //             if (q >= test.Length && d >= firstDialogue.Length)
+    //                 break;
+    //             if (d < firstDialogue.Length)
+    //                 dm.StartDialogue(firstDialogue[d..(d + 1)]);
+    //             if (q < test.Length)
+    //                 quizManager.StartQuiz(test[q..(q + 1)]);
+    //             playerScore += quizManager.NumCorrect;
+    //             totalScore += quizManager.NumQuestions;
+    //             q++;
+    //             d++;
+    //         }
+    //     else
+    //     {
+    //         while (true)
+    //         {
+    //             if (q >= test.Length && d >= firstDialogue.Length)
+    //                 break;
+    //             if (q < test.Length)
+    //                 quizManager.StartQuiz(test[q..(q + 1)]);
+    //             if (d < firstDialogue.Length)
+    //                 dm.StartDialogue(firstDialogue[d..(d + 1)]);
+    //             playerScore += quizManager.NumCorrect;
+    //             totalScore += quizManager.NumQuestions;
+    //             q++;
+    //             d++;
+    //         }
+    //     }
+    // }
 
     /*
 
-    private void OnQuizEnd() {
-        playerScore += quizManager.NumCorrect;
-        totalScore += quizManager.NumQuestions;
-        TriggerEvent();
-    }
+    
 
-    private void OnDialogueEnd() {
-        TriggerEvent();
-    }
-
-    public void TriggerEvent() {
-        switch(++currentEvent) {
-            case 1 : { // Trigger first dialogue
-                dialogueObj.GetComponent<DialogueManager>().StartDialogue(firstDialogue);
-                break;
-            }
-            case 2 : { // Trigger quiz
-                quizManager.StartQuiz(test);
-                break;
-            }
-            case 3 : { // Trigger dialogue after quiz, ending dialogue
-                dialogueObj.GetComponent<DialogueManager>().StartDialogue(secondDialogue);
-                break;
-            }
-            case 4 : { // End lesson
-                if((double)playerScore/totalScore >= .8)
-                    Holder.progress(1);
-                else
-                    Debug.Log("Player has failed the lesson.");
-                GetComponent<SceneSwap>().SwapScene("Lessons");
-                break;
-            }
-        }
-    }
+    
     */
 
 }
--- TocaTalk/Assets/Scripts/Lesson1/Trigger.cs
+++ TocaTalk/Assets/Scripts/Lesson1/Trigger.cs
@@ -8,7 +8,7 @@ public class Trigger : MonoBehaviour
     // When player walks into trigger, it triggers the events of the level
     public void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag == "Player") {
-            GameObject.FindWithTag("GameController").GetComponent<Events>().doThing(true);
+            GameObject.FindWithTag("GameController").GetComponent<Events>().NextDialogue();
             Destroy(this);
         }
     }
